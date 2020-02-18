457a760a7fad9e5f6148178a814dbd92c17468f7
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-02-18 17:53:51 +0900

    Java Thread Test in server

--- myTest/src/main/java/com/example/myTest/MyTestApplication.java
+++ myTest/src/main/java/com/example/myTest/MyTestApplication.java
@@ -3,12 +3,34 @@ package com.example.myTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class UpdateManage implements Runnable {
+	public void run() {
+		int idx = 0;
+		while (true) {
+			try {
+				if(idx++ > 5000)
+					return;
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return;
+			}
+			System.out.print("Update!\n");
+		}
+	} 
+}
+
 
 @SpringBootApplication
 public class MyTestApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyTestApplication.class, args);
+		Thread t = new Thread(new UpdateManage());
+		t.start();
 	}
 
 
