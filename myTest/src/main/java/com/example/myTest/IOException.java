7fa7de5218d045d0dda9bd46a02914493054c7cd
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-02-19 21:09:55 +0900

    Java https api 연동

---
+++ myTest/src/main/java/com/example/myTest/IOException.java
@@ -0,0 +1,5 @@
+package com.example.myTest;
+
+public class IOException {
+
+}
--- myTest/src/main/java/com/example/myTest/MyTestApplication.java
+++ myTest/src/main/java/com/example/myTest/MyTestApplication.java
@@ -1,14 +1,27 @@
 package com.example.myTest;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 class UpdateManage implements Runnable {
 	public void run() {
 		int idx = 0;
 		while (true) {
 			try {
-				if(idx++ > 5000)
+				if (idx++ > 5000)
 					return;
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -18,19 +31,52 @@ class UpdateManage implements Runnable {
 			}
 			System.out.print("Update!\n");
 		}
-	} 
+	}
 }
-
 
 @SpringBootApplication
 public class MyTestApplication {
 
+	public static void executePost(String targetURL) throws IOException {
+		URL yahoo;
+		try {
+			yahoo = new URL(targetURL);
 
+			URLConnection yc;
+			try {
+				yc = yahoo.openConnection();
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+				String inputLine;
+
+				while ((inputLine = in.readLine()) != null)
+					System.out.println(inputLine);
+				in.close();
+
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyTestApplication.class, args);
-		Thread t = new Thread(new UpdateManage());
-		t.start();
+
+		try {
+			executePost("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWAud");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//SpringApplication.run(MyTestApplication.class, args);
+		//Thread t = new Thread(new UpdateManage());
+		//t.start();
 	}
 
 
