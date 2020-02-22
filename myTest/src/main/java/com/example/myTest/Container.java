c0fff4050f613caa331b100edbe841a1438e69e7
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-02-22 21:26:48 +0900

    Container Class 생성 및 rateData 형식 map 타입 전역 변수 생성.

---
+++ myTest/src/main/java/com/example/myTest/Container.java
@@ -0,0 +1,32 @@
+package com.example.myTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Container {
+
+    Map<String,RateData> CountryRateContrainer = new HashMap<String,RateData>();
+
+    public Container(){
+
+    }
+    
+    public void DeContainer(){
+        CountryRateContrainer.clear();
+        CountryRateContrainer = null;
+    }
+
+    public void addData(String _Name,RateData _RateData){
+        CountryRateContrainer.put(_Name, _RateData);
+    }
+
+    public RateData getDataByName(String _Name) {
+        if(CountryRateContrainer.containsKey(_Name) != true)
+            return null;
+        else
+            return CountryRateContrainer.get(_Name);
+    }
+
+
+
+};
